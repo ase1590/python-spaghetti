ed5c4bbb139d465c7de84c9be53d33daf5650a0e
ase1590 <ase1590@hotmail.com>
2015-10-25 10:37:50 -0500

    Update divbad.py

--- divbad.py
+++ divbad.py
@@ -9,13 +9,17 @@ print(letters)
 a=float(input(n1))
 b=float(input(n2))
 
+# ##### DONT TOUCH ANYTHING BELOW LINE #####
+# #####  IT WORKS AND I DONT KNOW WHY  #####
 add_used = 0
 
+# define add
 def add(a, b):
     global add_used
     add_used += 1
     return a + b
 
+# dont know why this works but it does.
 def divide(a, b):
     quotient = 0
     c = 0
