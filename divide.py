70cc53d8eadc3b872b11c8453593fd6e3fdb6c5f
ase1590 <ase1590@hotmail.com>
2015-10-25 10:31:03 -0500

    Update divide.py

--- divide.py
+++ divide.py
@@ -4,8 +4,7 @@ print('divide two numbers')
 
 x=int(input('enter first number: '))
 y=int(input('enter number to divide by: '))
-#answer= x/y
-#remainder = x%y
+
 print('the answer is: ',int(x/y)),
 
 time.sleep(3) #delay of 5 seconds before closing
